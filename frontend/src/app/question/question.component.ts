0d479681874a73711416c2d3d14269299b385934
vamshikrishna24 <vamshikrishna2124@gmail.com>
2023-06-16 11:44:19 +0530

    question component modified 2

--- frontend/src/app/question/question.component.ts
+++ frontend/src/app/question/question.component.ts
@@ -7,6 +7,7 @@ import { timestamp } from 'rxjs';
   styleUrls: ['./question.component.css'],
 })
 export class QuestionComponent {
+  question: string = 'What will you choose from the below?';
   timeLeft: number = 60;
   interval: any;
   timerStarted: boolean = false;
@@ -14,55 +15,25 @@ export class QuestionComponent {
   crctOption: any;
   correctAnswerFlag: boolean = false;
   wrongAnswerFlag: boolean = false;
+  startFlag: boolean = false;
+  pauseFlag: boolean = false;
 
-  questions: any = [
-    {
-      id: 0,
-      question: 'this is question 1',
-      opt1: 'sample1',
-      opt2: 'sample2',
-      opt3: 'sample3',
-      opt4: 'sample4',
-      crct: 'sample3',
-      color: false,
-    },
-    {
-      id: 1,
-      question: 'this is question 2',
-      opt1: 'sample1',
-      opt2: 'sample2',
-      opt3: 'sample3',
-      opt4: 'sample4',
-      crct: 'sample2',
-      color: false,
-    },
-    {
-      id: 2,
-      question: 'this is question 3',
-      opt1: 'sample1',
-      opt2: 'sample2',
-      opt3: 'sample3',
-      opt4: 'sample4',
-      crct: 'sample1',
-      color: false,
-    },
-    {
-      id: 3,
-      question: 'this is question 4',
-      opt1: 'sample1',
-      opt2: 'sample2',
-      opt3: 'sample3',
-      opt4: 'sample4',
-      crct: 'sample4',
-      color: false,
-    },
+  options: any = [
+    { id: 0, name: 'sample1', crct: true, color: false },
+
+    { id: 1, name: 'option2', crct: false, color: false },
+
+    { id: 2, name: 'osample33', crct: false, color: false },
+
+    { id: 3, name: 'option4', crct: false, color: false },
   ];
-
   print(data: any) {
     console.log(data.name);
   }
 
   startTimer() {
+    this.startFlag = true;
+    this.pauseFlag = false;
     if (!this.timerStarted) {
       this.interval = setInterval(() => {
         if (this.timeLeft > 0) {
@@ -77,19 +48,21 @@ export class QuestionComponent {
   }
 
   pauseTimer() {
+    this.startFlag = false;
+    this.pauseFlag = true;
     this.timerStarted = false;
     clearInterval(this.interval);
   }
 
   changeColor(id: number) {
-    this.questions.forEach((element: any) => {
+    this.options.forEach((element: any) => {
       element.color = false;
       if (element.crct == true) {
         this.crctOption = element.id;
       }
     });
 
-    this.questions[id].color = !this.questions[id].color;
+    this.options[id].color = !this.options[id].color;
 
     this.selectedOption = id;
     console.log(this.crctOption);
